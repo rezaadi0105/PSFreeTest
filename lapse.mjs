223f1586cb4346ac265d28dd2272b1e1f111af05
rezaadi0105 <rezaadipangestu5@gmail.com>
2025-09-03 00:35:16 +0700

    Update & Rebase
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- lapse.mjs
+++ lapse.mjs
@@ -184,6 +184,14 @@ function call_nze(...args) {
     }
 }
 
+function allset() {
+    msgs.innerHTML="PS4 Exploited And Payload Loaded.";
+}
+
+function awaitpl() {
+    msgs.innerHTML="Payload Already Loaded, BinLoader Is Ready.<br>Send A Payload To Port 9020 Now";
+}
+
 // #define SCE_KERNEL_AIO_STATE_NOTIFIED       0x10000
 //
 // #define SCE_KERNEL_AIO_STATE_SUBMITTED      1
@@ -1715,7 +1723,7 @@ function runBinLoader() {
         payload_loader,
         payload_buffer
     );
-    log('BinLoader is ready. Send a payload to port 9020 now');
+    awaitpl();
 }
 
 // overview:
@@ -1912,6 +1920,9 @@ function loadPayload() {
 
             // Unmap the memory used for the payload
             sysi("munmap", payload_buffer, padded_buffer.length);
+
+            // Notification payload
+            allset();
           } catch (e) {
             // Caught error while trying to execute payload
             log(`error in loadPayload: ${e.message}`);
