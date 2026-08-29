faf6f136c9ca971319a10386138c516190ae8a8d
rezaadi0105 <rezaadipangestu5@gmail.com>
2026-08-29 10:24:39 +0700

    PSFree: Updates UI
    UI from psphive
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

---
+++ files/payloads/js/goldhen_versions.js
@@ -0,0 +1,49 @@
+var goldhenVersions = [
+  {
+    "value": "",
+    "text": "Select A Version From The List Below:",
+    "fw": ["9.xx","8.xx","7.xx"],
+    "disabled": true,
+    "selected": true
+  },
+  {
+    "value": "v2.4b18.12",
+    "text": "v2.4b18.12 - 22 September 2026 *Last Version*",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.11",
+    "text": "v2.4b18.11 - 20 September 2026",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.10",
+    "text": "v2.4b18.10 - 29 May 2026",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.9",
+    "text": "v2.4b18.9 - 12 February 2026",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.8",
+    "text": "v2.4b18.8 - 28 January 2026",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.7",
+    "text": "v2.4b18.7 - 24 November 2025",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.6",
+    "text": "v2.4b18.6 - 02 September 2025",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.5",
+    "text": "v2.4b18.5 - 04 August 2025",
+    "fw": ["9.xx","8.xx","7.xx"]
+  }
+];
